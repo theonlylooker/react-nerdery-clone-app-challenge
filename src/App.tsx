ad180e0032b3d18c1b11a5ba9288daaedecc1841
basurco eduardo <angelsoulsword@gmail.com>
2023-05-11 09:52:45 -0500

    feat: add github pages configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { createBrowserRouter } from "react-router-dom";
-import viteLogo from "/vite.svg";
-import "./App.css";
+//import { createBrowserRouter } from "react-router-dom";
+//import viteLogo from "/vite.svg";
+//import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
