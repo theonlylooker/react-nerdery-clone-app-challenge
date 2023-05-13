b3c7a5eaa15487521afa15d9a0cc5bc8f408fcef
basurco eduardo <angelsoulsword@gmail.com>
2023-05-13 11:33:29 -0500

    feat: add initial layout mobile

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
-const Layout = () => {
-  return <div>Layout</div>;
-};
+interface Layout {
+  display: string;
+  direction?: string;
+}
+
+const Layout = styled.div<Layout>`
+  display: ${(props) => `${props.display}`};
+  flex-direction: ${(props) =>
+    props.display === "flex" ? `${props.direction}` : ""};
+`;
+
+// const Layout = () => {
+//   return (
+//     <Mylayout>
+//       <div>asd</div>
+//       <div>asd</div>
+//     </Mylayout>
+//   );
+//};
 
 export default Layout;
