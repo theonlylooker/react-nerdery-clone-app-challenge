4582bc3df8d5f95818a1339c26a8f6b643cef571
basurco eduardo <angelsoulsword@gmail.com>
2023-05-13 12:07:13 -0500

    fix: fixed styles and components names

---
+++ src/cardGridContainer/cardGridContainer.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const CardGridContainter = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  @media (min-width: 550px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (min-width: 950px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  @media (min-width: 1120px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+  @media (min-width: 1640px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  }
+`;
+
+export default CardGridContainter;
--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -1,20 +1,5 @@
 import styled from "styled-components";
 
-const Content = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  @media (min-width: 550px) {
-    grid-template-columns: 1fr 1fr;
-  }
-  @media (min-width: 950px) {
-    grid-template-columns: 1fr 1fr 1fr;
-  }
-  @media (min-width: 1120px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-  }
-  @media (min-width: 1640px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  }
-`;
+const Content = styled.div``;
 
 export default Content;
