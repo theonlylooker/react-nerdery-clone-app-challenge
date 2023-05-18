59cb065c74075239fed41028cffd942cf90c3336
Eduardo Rolando Basurco Cayllahua <ebasurcoc@gmail.com>
2023-05-18 17:23:14 -0500

    Develop (#1)
    * feat: add initial files
    
    * feat: add style, routing and svg dependencies
    
    * feat: add vitest configuration
    
    * feat: add msw mocking configuration
    
    * feat: add github pages configuration
    
    * feat: add github actions
    
    * fix: change github token
    
    * fix: wrong branch name
    
    * fix: fixed github token
    
    * feat: testing develop branch
    
    * feat: changing from pages to vercel
    
    * fix: changing from emotion to styled components
    
    * feat: add global styles and theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,53 @@
+const colors = {
+  shade01: "#FFFFFF",
+  shade02: "#222222 ",
+  shade0205: "#22222205",
+  shade0230: "#22222230",
+  neutral01: "#F7F7F7",
+  neutral02: "#EBEBEB",
+  neutral03: "#DDDDDD",
+  neutral04: "#D3D3D3",
+  neutral05: "#C2C2C2",
+  neutral06: "#B0B0B0",
+  neutral07: "#717171",
+  neutral08: "#5E5E5E",
+  primary01: "#F6475F",
+  primary02: "#FF385C",
+  gradient01Step_01: "#C72D65",
+  gradient01Step_02: "#D23760",
+  gradient01Step_03: "#D23755",
+  error01: "#FEF086",
+  error02: "#C13515",
+  accent01: "#F6D7DF",
+  accent02: "#D03660",
+  discount: "#008A05",
+  link: "#004CC4",
+};
+
+const fontSize = {
+  xs: "10px",
+  s: "12px",
+  base: "13px",
+  m: "14px",
+  l: "16px",
+  xl: "18px",
+  xxl: "22px",
+};
+
+const weight = {
+  light: "590",
+  bold: "400",
+};
+
+const elevation = {
+  elevation01: "0px 4px 18px rgba(0, 0, 0, 0.17)",
+  elevation02: "box-shadow: 0px 1px 5px -363px rgba(0, 0, 0, 0.19)",
+  elevation03: "0px 6px 16px rgba(0, 0, 0, 0.12)",
+};
+
+export const theme = {
+  colors,
+  weight,
+  elevation,
+  fontSize,
+};
