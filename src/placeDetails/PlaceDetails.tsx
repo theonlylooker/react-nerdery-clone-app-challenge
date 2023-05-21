db78d09f1d213e062d29ab06735ba2869641dae2
basurco eduardo <angelsoulsword@gmail.com>
2023-05-21 12:12:08 -0500

    feat: add place header component UI

--- src/placeDetails/PlaceDetails.tsx
+++ src/placeDetails/PlaceDetails.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import PlaceHeader from "../placeHeader/PlaceHeader";
 
 const PlaceDetails = () => {
   const { id } = useParams();
   return (
     <div>
-      <div>Place Header component</div>
+      <PlaceHeader />
       <div>place Information component</div>
       <div>place Host component</div>
       <div>Place About component</div>
