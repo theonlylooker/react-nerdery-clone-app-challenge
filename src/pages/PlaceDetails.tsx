3b92960ef5e0dcc82af6a2b78f507b20d6f560b2
basurco eduardo <angelsoulsword@gmail.com>
2023-05-21 11:50:32 -0500

    feat: add basic place details layout

--- src/pages/PlaceDetails.tsx
+++ src/pages/PlaceDetails.tsx
@@ -3,7 +3,17 @@ import { useParams } from "react-router-dom";
 
 const PlaceDetails = () => {
   const { id } = useParams();
-  return <div>{id}</div>;
+  return (
+    <div>
+      <div>Place Header component</div>
+      <div>place Information component</div>
+      <div>place Host component</div>
+      <div>Place About component</div>
+      <div style={{ position: "fixed", bottom: 0 }}>
+        Place Date Separation component
+      </div>
+    </div>
+  );
 };
 
 export default PlaceDetails;
