447d17ab748705857404a79198573ddf2a63b674
basurco eduardo <angelsoulsword@gmail.com>
2023-05-21 12:25:56 -0500

    feat: add basic layout

---
+++ src/placeInformation/PlaceInformation.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PlaceInformation = () => {
+  return (
+    <div>
+      <div>image</div>
+      <div>place name</div>
+      <div>
+        <div>rating</div>
+        <div>reseñas</div>
+        <div>Ubicacion</div>
+      </div>
+      <div>
+        <div>name - anfitrion name</div>
+        <div>host icon</div>
+        <div>main features</div>
+        <div>type features</div>
+      </div>
+    </div>
+  );
+};
+
+export default PlaceInformation;
