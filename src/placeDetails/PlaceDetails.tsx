28926a7da01269448a1be822fe5e28a912848807
basurco eduardo <angelsoulsword@gmail.com>
2023-05-22 13:12:48 -0500

    feat: add basic UI

--- src/placeDetails/PlaceDetails.tsx
+++ src/placeDetails/PlaceDetails.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import PlaceHeader from "../placeHeader/PlaceHeader";
 import PlaceDate from "../placeDate/PlaceDate";
+import PlaceHost from "../placeHost/PlaceHost";
 
 const PlaceDetails = () => {
   const { id } = useParams();
@@ -9,7 +10,7 @@ const PlaceDetails = () => {
     <div>
       <PlaceHeader />
       <div>place Information component</div>
-      <div>place Host component</div>
+      <PlaceHost />
       <div>Place About component</div>
       <div style={{ position: "fixed", bottom: 0, width: "100%" }}>
         <PlaceDate />
