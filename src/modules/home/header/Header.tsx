8fb99034e016f523d5094cd8b5723262fd8a6c24
basurco eduardo <angelsoulsword@gmail.com>
2023-06-08 15:44:12 -0500

    misc: change identation

--- src/modules/home/header/Header.tsx
+++ src/modules/home/header/Header.tsx
@@ -3,14 +3,11 @@ import { Search } from "../search/Search";
 import { FilterCarrousel } from "../filterCarrousel/FilterCarrousel";
 import { HeaderI } from "./styles";
 
-interface Header {
-  handleCategory: (category: string) => void;
-}
-export const Header: FC<Header> = ({ handleCategory }) => {
+export const Header: FC = () => {
   return (
     <HeaderI>
       <Search />
-      <FilterCarrousel handleCategory={handleCategory} />
+      <FilterCarrousel />
     </HeaderI>
   );
 };
