3ded2417728bcd54204b8f5b34cde5ee6ed29501
basurco eduardo <angelsoulsword@gmail.com>
2023-06-14 09:45:24 -0500

    feat: add user modal auth links

--- src/modules/home/userModal/UserModal.tsx
+++ src/modules/home/userModal/UserModal.tsx
@@ -1,6 +1,8 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { UserContext, useUserContext } from "../../../context/UserContext";
+import { WishlistContext } from "../../../context/WishlistContext";
 
 const UserModalLayout = styled.div`
   padding: 8px 0;
@@ -26,6 +28,24 @@ const UserModalItem = styled(Link)<{ primary?: boolean }>`
     background-color: ${({ theme }) => `${theme.colors.neutral01}`};
   }
 `;
+const UserModalButton = styled.button<{ primary?: boolean }>`
+  border-radius: 10px;
+  padding: 8px 8px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  display: block;
+  text-decoration: none;
+  background-color: inherit;
+  cursor: pointer;
+  font-size: ${({ theme }) => `${theme.fontSize.l}`};
+  color: ${({ theme, primary }) =>
+    `${primary ? `${theme.colors.shade02}` : `${theme.colors.neutral08}`} `};
+
+  :hover {
+    background-color: ${({ theme }) => `${theme.colors.neutral01}`};
+  }
+`;
 
 const UserModalAction = styled.div`
   box-shadow: ${({ theme }) => `0 1px 2px -2px ${theme.colors.neutral08}`};
@@ -37,13 +57,33 @@ interface UserModal {
   handleModal: () => void;
 }
 export const UserModal: FC<UserModal> = () => {
+  const { user, setUser } = useContext(UserContext);
+  const { setWishlist } = useContext(WishlistContext);
+  const logout = () => {
+    localStorage.removeItem("airbnbToken");
+    localStorage.removeItem("airbnbUser");
+    setUser(null);
+    setWishlist([]);
+    location.reload();
+  };
   return (
     <UserModalLayout>
       <UserModalAction>
-        <UserModalItem to={"/login"} primary={true}>
-          Log in
-        </UserModalItem>
-        <UserModalItem to={"/login"}>Sign up</UserModalItem>
+        {user ? (
+          <>
+            <UserModalButton primary={true} onClick={logout}>
+              Log out
+            </UserModalButton>
+            <UserModalItem to={"/wishlist"}>Wishlists </UserModalItem>
+          </>
+        ) : (
+          <>
+            <UserModalItem to={"/login"} primary={true}>
+              Log in
+            </UserModalItem>
+            <UserModalItem to={"/login"}>Sign up</UserModalItem>
+          </>
+        )}
       </UserModalAction>
       <div>
         <UserModalItem to={"#"}>Airbnb your home</UserModalItem>
